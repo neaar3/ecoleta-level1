fdb7a3099dc508ca8469626174fb72ef05716456
Neaar3 <55645238+neaar3@users.noreply.github.com>
2020-06-07 21:38:36 -0300

    Add files via upload

---
+++ public/scripts/intro.js
@@ -0,0 +1,69 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "hello"
+// const myconst = "he"
+
+// document.write(myconst + myvar)
+
+
+//string
+// const s1 = "Isso é uma string"
+// const s2 = 'isso também é uma string'
+// const s3 = `isso é uma string tbm`
+
+// document.write(s2)
+
+// const n1 = 1
+// const n2 = 12
+
+// document.write(n1 + n2)
+
+//boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+//objetos possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// funções - funcionalidades - atalhos - reuso de codigo
+//registrar função
+// function sayMyName(name) {
+
+//   document.write(name)
+// }
+
+// //executar
+// sayMyName("lucas")
+
+//condições
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+// se colocar crase da pra por variavel, sem crase n pega variavel
+// for (i = 0; i < 10; i++) {
+//     document.write(`<p>${i}</p>`)
+//   }
